ee0148c8882d4e47b5373a64d1c3f56365a12c40
somshk <89773606+somshk@users.noreply.github.com>
2026-05-27 23:52:32 +0800

    Update config for Render environment variables

--- lssite/lssite/config.py
+++ lssite/lssite/config.py
@@ -1,24 +1,14 @@
-# config.py
 import os
 from pathlib import Path
 import environ
-import io
-from google.cloud import secretmanager
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env = environ.Env(DEBUG=(bool, False))
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
 env_file = os.path.join(BASE_DIR, ".env")
 
 if os.path.isfile(env_file):
     env.read_env(env_file)
-    
-elif os.environ.get("GOOGLE_CLOUD_PROJECT", None):
-    project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")
-    client = secretmanager.SecretManagerServiceClient()
-    settings_name = os.environ.get("SETTINGS_NAME", "django_settings")
-    name = f"projects/{project_id}/secrets/{settings_name}/versions/latest"
-    payload = client.access_secret_version(name=name).payload.data.decode("UTF-8")
-    env.read_env(io.StringIO(payload))
-else:
-    raise Exception("No local .env or GOOGLE_CLOUD_PROJECT detected. No secrets found.")
